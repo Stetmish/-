0dfd4af887a7d057c86b0c2d34ae9791fe615b5d
Stetmish <89745441+Stetmish@users.noreply.github.com>
2021-08-30 02:39:51 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,23 +1,23 @@
 #include <math.h>
 #include <stdio.h>
-double linear_root(double b,double c){
+double linear_root(double b, double c){
 	double x = 0;
 	x = c*(-1)/b;
 	return x;
 }
-double first_quadratic_root(double a,double b,double c){
+double first_quadratic_root(double a, double b, double c){
 	double x1 = 0, D = 0;
 	D = b*b-4*a*c;
 	x1 = (b*(-1)+sqrt(D))/(2*a);
 	return x1;
 }
-double second_quadratic_root(double a,double b,double c){
+double second_quadratic_root(double a, double b, double c){
 	double x2 = 0, D = 0;
 	D = b*b-4*a*c;
 	x2 = (b*(-1)-sqrt(D))/(2*a);
 	return x2;
 }
-int solution(double a,double b,double c){
+int solution(double a, double b, double c){
 	if(a == 0 && b == 0){
 		if(c == 0){
 			return 1;
@@ -38,7 +38,7 @@ int solution(double a,double b,double c){
 		}
 	}
 }
-void output(int s,double a,double b,double c){
+void output(int s, double a, double b, double c){
 	if(s == 1){
 		printf("R");
 	}
@@ -46,24 +46,24 @@ void output(int s,double a,double b,double c){
 		printf("No roots");
 	}
 	else if(s == 3){
-		printf("%.6lf",linear_root(b,c));
+		printf("%.6lf", linear_root(b, c));
 	}
 	else if(s == 4){
-		if(first_quadratic_root(a,b,c) == second_quadratic_root(a,b,c)){
-			printf("%.6lf",first_quadratic_root(a,b,c));
+		if(first_quadratic_root(a, b, c) == second_quadratic_root(a, b, c)){
+			printf("%.6lf", first_quadratic_root(a, b, c));
 		}
 		else{
-			printf("%.6lf",first_quadratic_root(a,b,c));
+			printf("%.6lf", first_quadratic_root(a, b, c));
 			printf(" ");
-			printf("%.6lf",second_quadratic_root(a,b,c));
+			printf("%.6lf", second_quadratic_root(a, b, c));
 		}
 	}	
 }
 int main () {
 	double a = 0, b = 0, c = 0;
-	scanf("%lf",&a);
-	scanf("%lf",&b);
-	scanf("%lf",&c);
-	output(solution(a,b,c),a,b,c);
+	scanf("%lf", &a);
+	scanf("%lf", &b);
+	scanf("%lf", &c);
+	output(solution(a, b, c), a, b, c);
 	return 0;
 }
