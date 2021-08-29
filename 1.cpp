d51bc414e627133a2417dfc34875badb16820057
Stetmish <89745441+Stetmish@users.noreply.github.com>
2021-08-30 01:15:17 +0300

    Update 1.cpp

--- 1.cpp
+++ 1.cpp
@@ -1,49 +1,69 @@
 #include <math.h>
 #include <stdio.h>
-double x1,x2,x,D;
-void linear_equation(double b,double c){
-x=c*(-1)/b;
+double D;
+double linear_equation(double b,double c){
+	double x;
+	x=c*(-1)/b;
+	return x;
 }
-void quadratic_equation(double a,double b,double c){
-x1=(-b+sqrt(D))/(2*a);
-x2=(-b-sqrt(D))/(2*a);
+double quadratic_equation1(double a,double b,double c){
+	double x1;
+	x1=(b*(-1)+sqrt(D))/(2*a);
+	return x1;
 }
-void solution(double a,double b,double c){
-if(a==0 && b==0){
-if(c==0){
- printf("R");
+double quadratic_equation2(double a,double b,double c){
+	double x2;
+	x2=(b*(-1)-sqrt(D))/(2*a);
+	return x2;
 }
-else{
-printf("No roots");
+int solution(double a,double b,double c){
+	if(a==0 && b==0){
+	if(c==0){
+	return 1;
+	}
+	else{
+	return 2;
+	}
+	}
+	else if(a==0){
+	return 3;
+	}
+	else{
+	D=b*b-4*a*c;
+	if(D<0){
+	return 2;	
+	}
+	else{
+		return 4;
+	}
 }
-}
-else if(a==0){
-linear_equation(b,c);
-printf("%.6lf",x);
-}
-else{
-D=b*b-4*a*c;
-if(D<0){
-printf("No roots");	
-}
-else{
-quadratic_equation(a,b,c);
-if(x1==x2){
-printf("%.6lf",x1);
-}
-else{
-printf("%.6lf",x1);
-printf(" ");
-printf("%.6lf",x2);
-}
-}
-}
+void output(int a){
+	if(a==1){
+	printf("R");
+	}
+	else if(a==2){
+	printf("No roots");
+	}
+	else if(a==3){
+	printf("%.6lf",linear_equation(b,c));
+	}
+	else if(a==4){
+	if(quadratic_equation1(a,b,c)==quadratic_equation2(a,b,c)){
+	printf("%.6lf",quadratic_equation1(a,b,c));
+	}
+	else{
+	printf("%.6lf",quadratic_equation1(a,b,c));
+	printf(" ");
+	printf("%.6lf",quadratic_equation2(a,b,c));
+	}
+	}	
+	}
 }
 int main () {
-double a,b,c;
+double a=0,b=0,c=0;
 scanf("%lf",&a);
 scanf("%lf",&b);
 scanf("%lf",&c);
-solution(a,b,c);
+output(solution(a,b,c));
 return 0;
 }
